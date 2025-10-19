55b7c8b6792f40fd38728f266b817c445b903bc1
Nada Ahmed <nadanaga9@gmail.com>
2025-10-19 21:15:45 +0300

    register style

--- src/app/Components/register/register.ts
+++ src/app/Components/register/register.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../Services/auth-service';
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
